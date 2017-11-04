9baea53092e3641eff02d1e1d594c27044842e8c
Nicolamber <33043378+Nicolamber@users.noreply.github.com>
2017-11-04 09:13:13 -0300

    inicial

---
+++ Desercion.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <title>Abandono de estudio</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row"></div>
+            <div class="col-md-2"> </div>
+            <div class="col-md-8"> </div>
+             <div class="jumbotron">
+            <form class="navbar-form navbar" role="search">
+                <a href="http://localhost/Encuesta/index.php" class="btn btn-primary">Inicio</a>
+                <a href="http://localhost/Encuesta/Admision.php" class="btn btn-primary">Admision</a>
+                <a href="http://localhost/Encuesta/MitadCarrera.php" class="btn btn-primary">Mitad Carrera</a>
+                <a href="http://localhost/Encuesta/Titulacion.php" class="btn btn-primary">Titulacion</a>
+                <a href="http://localhost/Encuesta/Laboral.php" class="btn btn-primary">Laboral</a>
+                <a href="http://localhost/Encuesta/Progreso.php" class="btn btn-primary">Progreso laboral</a>
+               
+                    <h1>El que abandona no tiene premio...</h1>
+                    <h3>Es una pena que ya no estes con nosotros pero igual te dejamos una encuesta para saber el porque</h3>
+
+            <br><div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">Pregunta 1: </span>
+                <input type="text" class="form-control" placeholder="..." aria-describedby="basic-addon1">
+            </div></br>
+            <br></br>
+            <br><div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">Pregunta 2: </span>
+                <input type="text" class="form-control" placeholder="..." aria-describedby="basic-addon1">
+            </div></br>
+            <br></br>
+            <br><div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">Pregunta 3: </span>
+                <input type="text" class="form-control" placeholder="..." aria-describedby="basic-addon1">
+            </div></br>
+            <div class="col-md-2"> </div>
+        </div>
+            </div>
+    </body>
+</html>
