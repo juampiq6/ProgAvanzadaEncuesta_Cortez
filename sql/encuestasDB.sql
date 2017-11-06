57cb74070c78dddfbeede10475a03d09cf5ad246
Juampi Quinteros <juampiq6@gmail.com>
2017-11-05 22:52:33 -0300

    mejorado bd y adjunto grafico bd

--- sql/encuestasDB.sql
+++ sql/encuestasDB.sql
@@ -1,354 +1,409 @@
-﻿SET FOREIGN_KEY_CHECKS = 0;
+﻿-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-11-2017 a las 02:45:22
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 7.1.1
+
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-CREATE DATABASE IF NOT EXISTS encuestasDB;
-USE encuestasDB;
-CREATE TABLE `domicilio`(
-    `idDomicilio` INT(11) NOT NULL,
-    `Calle` VARCHAR(45) NOT NULL,
-    `Numero` INT(11) DEFAULT NULL,
-    `Dpto` VARCHAR(45) DEFAULT NULL,
-    `Piso` VARCHAR(45) DEFAULT NULL,
-    `FkLocalidad` INT(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS encuestasdb;USE encuestasdb;--
+-- Base de datos: `encuestasdb`
+--
+
 -- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `domicilio`
 --
 
--- Estructura de tabla para la tabla `encuesta`
---
+CREATE TABLE `domicilio` (
+  `idDomicilio` int(11) NOT NULL,
+  `calle` varchar(45) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `dpto` varchar(45) DEFAULT NULL,
+  `piso` varchar(45) DEFAULT NULL,
+  `fk_localidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `encuesta`(
-    `idEncuesta` INT(11) NOT NULL,
-    `EncuestaNomb` VARCHAR(45) NOT NULL,
-    `Descripcion` VARCHAR(200) NOT NULL,
-    `FkPersonaCuil` INT(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 -- --------------------------------------------------------
---
 
+--
 -- Estructura de tabla para la tabla `estadocivil`
 --
 
-CREATE TABLE `estadocivil`(
-    `idEstadoCivil` INT(11) NOT NULL,
-    `NomEstCiv` VARCHAR(20) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
--- --------------------------------------------------------
---
+CREATE TABLE `estadocivil` (
+  `idEstadoCivil` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `genero`
 --
 
-CREATE TABLE `genero`(
-    `idGenero` INT(11) NOT NULL,
-    `NombreGenero` VARCHAR(20) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
--- --------------------------------------------------------
---
+CREATE TABLE `genero` (
+  `idGenero` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `localidad`
 --
 
-CREATE TABLE `localidad`(
-    `idLocalidad` INT(11) NOT NULL,
-    `NomLocalidad` VARCHAR(150) DEFAULT NULL,
-    `FkProvincia` INT(11) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
--- --------------------------------------------------------
---
+CREATE TABLE `localidad` (
+  `idLocalidad` int(11) NOT NULL,
+  `nombre` varchar(150) DEFAULT NULL,
+  `fk_provincia` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pais`
 --
 
-CREATE TABLE `pais`(
-    `idPais` INT(11) NOT NULL,
-    `NomPais` VARCHAR(150) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
--- --------------------------------------------------------
---
+CREATE TABLE `pais` (
+  `idPais` int(11) NOT NULL,
+  `nombre` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `persona`
 --
 
-CREATE TABLE `persona`(
-    `idPersona` INT(11) NOT NULL,
-    `Nom` VARCHAR(45) NOT NULL,
-    `Apellido` VARCHAR(45) NOT NULL,
-    `ApellidoMaterno` VARCHAR(45) DEFAULT NULL,
-    `DNI` INT(8) NOT NULL,
-    `CUIL` INT(11) NOT NULL,
-    `FechNac` DATE NOT NULL,
-    `FkLugarNac` INT(11) DEFAULT NULL,
-    `FkEstadoCivil` INT(11) NOT NULL,
-    `FkGenero` INT(11) NOT NULL,
-    `FkDomicilio` INT(11) NOT NULL,
-    `PersonaTipo` VARCHAR(45) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
--- --------------------------------------------------------
---
+CREATE TABLE `persona` (
+  `idPersona` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `apellidoMaterno` varchar(45) DEFAULT NULL,
+  `dni` int(8) NOT NULL,
+  `cuit` int(11) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `fk_lugarNac` int(11) DEFAULT NULL,
+  `fk_estadoCivil` int(11) NOT NULL,
+  `fk_genero` int(11) NOT NULL,
+  `fk_domicilio` int(11) NOT NULL,
+  `personaTipo` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pregunta`
 --
 
-CREATE TABLE `pregunta`(
-    `idPregunta` INT(11) NOT NULL,
-    `Pregunta` VARCHAR(200) NOT NULL,
-    `Respuesta` VARCHAR(200) DEFAULT NULL,
-    `FKEncuesta` INT(11) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
--- --------------------------------------------------------
---
+CREATE TABLE `pregunta` (
+  `idPregunta` int(11) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `fk_tipoencuesta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `provincia`
 --
 
-CREATE TABLE `provincia`(
-    `idProvincia` INT(11) NOT NULL,
-    `NomProvincia` VARCHAR(150) DEFAULT NULL,
-    `FkPais` INT(11) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE `provincia` (
+  `idProvincia` int(11) NOT NULL,
+  `nombre` varchar(150) DEFAULT NULL,
+  `fk_pais` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta`
 --
 
+CREATE TABLE `respuesta` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `fk_user` int(11) NOT NULL,
+  `fk_pregunta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipoencuesta`
+--
+
+CREATE TABLE `tipoencuesta` (
+  `idTipoEncuesta` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user`
 --
 
-CREATE TABLE `user`(
-    `idUser` INT(11) NOT NULL,
-    `NombreUsuario` VARCHAR(45) NOT NULL,
-    `Contraseña` VARCHAR(45) NOT NULL,
-    `FkPersona` INT(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL,
+  `nombreUsuario` varchar(45) NOT NULL,
+  `contraseña` varchar(45) NOT NULL,
+  `fk_persona` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_tipoenc`
 --
 
+CREATE TABLE `user_tipoenc` (
+  `idUser_TipoEnc` int(11) NOT NULL,
+  `fk_user` int(11) NOT NULL,
+  `fk_tipoEncuesta` int(11) NOT NULL,
+  `completa` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
 -- Índices para tablas volcadas
 --
 
 --
-
 -- Indices de la tabla `domicilio`
 --
+ALTER TABLE `domicilio`
+  ADD PRIMARY KEY (`idDomicilio`),
+  ADD UNIQUE KEY `idDomicilio_UNIQUE` (`idDomicilio`),
+  ADD KEY `FkLocalidad_idx` (`fk_localidad`);
 
-ALTER TABLE
-    `domicilio` ADD PRIMARY KEY(`idDomicilio`),
-    ADD UNIQUE KEY `idDomicilio_UNIQUE`(`idDomicilio`),
-    ADD KEY `FkLocalidad_idx`(`FkLocalidad`);
-    --
+--
+-- Indices de la tabla `estadocivil`
+--
+ALTER TABLE `estadocivil`
+  ADD PRIMARY KEY (`idEstadoCivil`),
+  ADD UNIQUE KEY `idEstadoCivil_UNIQUE` (`idEstadoCivil`);
 
-    -- Indices de la tabla `encuesta`
-    --
+--
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`idGenero`),
+  ADD UNIQUE KEY `idPersona_UNIQUE` (`idGenero`);
 
-ALTER TABLE
-    `encuesta` ADD PRIMARY KEY(`idEncuesta`),
-    ADD UNIQUE KEY `idEncuesta_UNIQUE`(`idEncuesta`),
-    ADD KEY `FkPersona_idx`(`FkPersonaCuil`);
-    --
+--
+-- Indices de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  ADD PRIMARY KEY (`idLocalidad`),
+  ADD UNIQUE KEY `idLocalidad_UNIQUE` (`idLocalidad`),
+  ADD KEY `FkProvincia_idx` (`fk_provincia`);
 
-    -- Indices de la tabla `estadocivil`
-    --
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`idPais`),
+  ADD UNIQUE KEY `idPais_UNIQUE` (`idPais`);
 
-ALTER TABLE
-    `estadocivil` ADD PRIMARY KEY(`idEstadoCivil`),
-    ADD UNIQUE KEY `idEstadoCivil_UNIQUE`(`idEstadoCivil`);
-    --
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`cuit`),
+  ADD UNIQUE KEY `idPersona_UNIQUE` (`idPersona`),
+  ADD KEY `FkLugarNac_idx` (`fk_lugarNac`),
+  ADD KEY `FkEstadoCivil_idx` (`fk_estadoCivil`),
+  ADD KEY `FkGenero_idx` (`fk_genero`),
+  ADD KEY `FkDomicilio_idx` (`fk_domicilio`);
 
-    -- Indices de la tabla `genero`
-    --
+--
+-- Indices de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD PRIMARY KEY (`idPregunta`),
+  ADD UNIQUE KEY `idPregunta_UNIQUE` (`idPregunta`),
+  ADD KEY `fk_tipoencuesta` (`fk_tipoencuesta`);
 
-ALTER TABLE
-    `genero` ADD PRIMARY KEY(`idGenero`),
-    ADD UNIQUE KEY `idPersona_UNIQUE`(`idGenero`);
-    --
+--
+-- Indices de la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  ADD PRIMARY KEY (`idProvincia`),
+  ADD UNIQUE KEY `idProvincia_UNIQUE` (`idProvincia`),
+  ADD KEY `FkPais_idx` (`fk_pais`);
 
-    -- Indices de la tabla `localidad`
-    --
+--
+-- Indices de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user` (`fk_user`),
+  ADD KEY `fk_pregunta` (`fk_pregunta`);
 
-ALTER TABLE
-    `localidad` ADD PRIMARY KEY(`idLocalidad`),
-    ADD UNIQUE KEY `idLocalidad_UNIQUE`(`idLocalidad`),
-    ADD KEY `FkProvincia_idx`(`FkProvincia`);
-    --
+--
+-- Indices de la tabla `tipoencuesta`
+--
+ALTER TABLE `tipoencuesta`
+  ADD PRIMARY KEY (`idTipoEncuesta`),
+  ADD KEY `nombre` (`nombre`);
 
-    -- Indices de la tabla `pais`
-    --
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `idUser_UNIQUE` (`idUser`),
+  ADD UNIQUE KEY `NombreUsuario` (`nombreUsuario`),
+  ADD UNIQUE KEY `idUser` (`idUser`),
+  ADD KEY `FkPersona_idx` (`fk_persona`),
+  ADD KEY `NombreUsuario_2` (`nombreUsuario`);
 
-ALTER TABLE
-    `pais` ADD PRIMARY KEY(`idPais`),
-    ADD UNIQUE KEY `idPais_UNIQUE`(`idPais`);
-    --
+--
+-- Indices de la tabla `user_tipoenc`
+--
+ALTER TABLE `user_tipoenc`
+  ADD PRIMARY KEY (`idUser_TipoEnc`),
+  ADD KEY `fk_user` (`fk_user`),
+  ADD KEY `fk_tipoEncuesta` (`fk_tipoEncuesta`);
 
-    -- Indices de la tabla `persona`
-    --
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
-ALTER TABLE
-    `persona` ADD PRIMARY KEY(`CUIL`),
-    ADD UNIQUE KEY `idPersona_UNIQUE`(`idPersona`),
-    ADD KEY `FkLugarNac_idx`(`FkLugarNac`),
-    ADD KEY `FkEstadoCivil_idx`(`FkEstadoCivil`),
-    ADD KEY `FkGenero_idx`(`FkGenero`),
-    ADD KEY `FkDomicilio_idx`(`FkDomicilio`);
-    --
+--
+-- AUTO_INCREMENT de la tabla `domicilio`
+--
+ALTER TABLE `domicilio`
+  MODIFY `idDomicilio` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `estadocivil`
+--
+ALTER TABLE `estadocivil`
+  MODIFY `idEstadoCivil` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `idGenero` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  MODIFY `idLocalidad` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `idPais` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `idPersona` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  MODIFY `idPregunta` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  MODIFY `idProvincia` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tipoencuesta`
+--
+ALTER TABLE `tipoencuesta`
+  MODIFY `idTipoEncuesta` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `user_tipoenc`
+--
+ALTER TABLE `user_tipoenc`
+  MODIFY `idUser_TipoEnc` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
 
-    -- Indices de la tabla `pregunta`
-    --
+--
+-- Filtros para la tabla `domicilio`
+--
+ALTER TABLE `domicilio`
+  ADD CONSTRAINT `domicilio_ibfk_1` FOREIGN KEY (`fk_localidad`) REFERENCES `localidad` (`idLocalidad`);
 
-ALTER TABLE
-    `pregunta` ADD PRIMARY KEY(`idPregunta`),
-    ADD UNIQUE KEY `idPregunta_UNIQUE`(`idPregunta`),
-    ADD KEY `FKEncuesta_idx`(`FKEncuesta`);
-    --
+--
+-- Filtros para la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  ADD CONSTRAINT `localidad_ibfk_1` FOREIGN KEY (`fk_provincia`) REFERENCES `provincia` (`idProvincia`);
 
-    -- Indices de la tabla `provincia`
-    --
+--
+-- Filtros para la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `FkDomicilio` FOREIGN KEY (`fk_domicilio`) REFERENCES `domicilio` (`idDomicilio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FkEstadoCivil` FOREIGN KEY (`fk_estadoCivil`) REFERENCES `estadocivil` (`idEstadoCivil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FkGenero` FOREIGN KEY (`fk_genero`) REFERENCES `genero` (`idGenero`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FkLugarNac` FOREIGN KEY (`fk_lugarNac`) REFERENCES `localidad` (`idLocalidad`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE
-    `provincia` ADD PRIMARY KEY(`idProvincia`),
-    ADD UNIQUE KEY `idProvincia_UNIQUE`(`idProvincia`),
-    ADD KEY `FkPais_idx`(`FkPais`);
-    --
+--
+-- Filtros para la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD CONSTRAINT `pregunta_ibfk_1` FOREIGN KEY (`fk_tipoencuesta`) REFERENCES `tipoencuesta` (`idTipoEncuesta`);
 
-    -- Indices de la tabla `user`
-    --
+--
+-- Filtros para la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  ADD CONSTRAINT `provincia_ibfk_1` FOREIGN KEY (`fk_pais`) REFERENCES `pais` (`idPais`);
 
-ALTER TABLE
-    `user` ADD PRIMARY KEY(`NombreUsuario`),
-    ADD UNIQUE KEY `idUser_UNIQUE`(`idUser`),
-    ADD KEY `FkPersona_idx`(`FkPersona`);
-    --
+--
+-- Filtros para la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `user` (`idUser`),
+  ADD CONSTRAINT `respuesta_ibfk_2` FOREIGN KEY (`fk_pregunta`) REFERENCES `pregunta` (`idPregunta`);
 
-    -- AUTO_INCREMENT de las tablas volcadas
-    --
+--
+-- Filtros para la tabla `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `FkPersona` FOREIGN KEY (`fk_persona`) REFERENCES `persona` (`cuit`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user/tipoenc` (`fk_user`);
 
-    --
+--
+-- Filtros para la tabla `user_tipoenc`
+--
+ALTER TABLE `user_tipoenc`
+  ADD CONSTRAINT `user_tipoenc_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `user` (`idUser`),
+  ADD CONSTRAINT `user_tipoenc_ibfk_2` FOREIGN KEY (`fk_tipoEncuesta`) REFERENCES `tipoencuesta` (`idTipoEncuesta`);
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
 
-    -- AUTO_INCREMENT de la tabla `domicilio`
-    --
-
-ALTER TABLE
-    `domicilio` MODIFY `idDomicilio` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `encuesta`
-    --
-
-ALTER TABLE
-    `encuesta` MODIFY `idEncuesta` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `estadocivil`
-    --
-
-ALTER TABLE
-    `estadocivil` MODIFY `idEstadoCivil` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `genero`
-    --
-
-ALTER TABLE
-    `genero` MODIFY `idGenero` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `localidad`
-    --
-
-ALTER TABLE
-    `localidad` MODIFY `idLocalidad` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `pais`
-    --
-
-ALTER TABLE
-    `pais` MODIFY `idPais` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `persona`
-    --
-
-ALTER TABLE
-    `persona` MODIFY `idPersona` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `pregunta`
-    --
-
-ALTER TABLE
-    `pregunta` MODIFY `idPregunta` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `provincia`
-    --
-
-ALTER TABLE
-    `provincia` MODIFY `idProvincia` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- AUTO_INCREMENT de la tabla `user`
-    --
-
-ALTER TABLE
-    `user` MODIFY `idUser` INT(11) NOT NULL AUTO_INCREMENT;
-    --
-
-    -- Restricciones para tablas volcadas
-    --
-
-    --
-
-    -- Filtros para la tabla `domicilio`
-    --
-
-ALTER TABLE
-    `domicilio` ADD CONSTRAINT `FkLocalidad` FOREIGN KEY(`FkLocalidad`) REFERENCES `localidad`(`idLocalidad`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-    --
-
-    -- Filtros para la tabla `encuesta`
-    --
-
-ALTER TABLE
-    `encuesta` ADD CONSTRAINT `FkPersonaCuil` FOREIGN KEY(`FkPersonaCuil`) REFERENCES `persona`(`CUIL`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-    --
-
-    -- Filtros para la tabla `localidad`
-    --
-
-ALTER TABLE
-    `localidad` ADD CONSTRAINT `FkProvincia` FOREIGN KEY(`FkProvincia`) REFERENCES `provincia`(`idProvincia`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-    --
-
-    -- Filtros para la tabla `persona`
-    --
-
-ALTER TABLE
-    `persona` ADD CONSTRAINT `FkDomicilio` FOREIGN KEY(`FkDomicilio`) REFERENCES `domicilio`(`idDomicilio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    ADD CONSTRAINT `FkEstadoCivil` FOREIGN KEY(`FkEstadoCivil`) REFERENCES `estadocivil`(`idEstadoCivil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    ADD CONSTRAINT `FkGenero` FOREIGN KEY(`FkGenero`) REFERENCES `genero`(`idGenero`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    ADD CONSTRAINT `FkLugarNac` FOREIGN KEY(`FkLugarNac`) REFERENCES `localidad`(`idLocalidad`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-    --
-
-    -- Filtros para la tabla `pregunta`
-    --
-
-ALTER TABLE
-    `pregunta` ADD CONSTRAINT `FKEncuesta` FOREIGN KEY(`FKEncuesta`) REFERENCES `encuesta`(`idEncuesta`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-    --
-
-    -- Filtros para la tabla `provincia`
-    --
-
-ALTER TABLE
-    `provincia` ADD CONSTRAINT `FkPais` FOREIGN KEY(`FkPais`) REFERENCES `pais`(`idPais`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-    --
-
-    -- Filtros para la tabla `user`
-    --
-
-ALTER TABLE
-    `user` ADD CONSTRAINT `FkPersona` FOREIGN KEY(`FkPersona`) REFERENCES `persona`(`CUIL`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-SET
-    FOREIGN_KEY_CHECKS = 1;
-COMMIT
-    ;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
